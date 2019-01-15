718a8f05c67b0e151508c6940d5205705d50dd23
Mrinal Vashisth <mrinalvashisth@rocketmail.com>
2019-01-15 19:26:03 +0530

    Update phonebook.py
    Implemented a phonebook, now in testing phase.

--- phonebook.py
+++ phonebook.py
@@ -1,161 +1,121 @@
+#!/usr/bin/env python
+
+import os
 import sys
 
+USAGE = """
+Usage:
+    python phonebook.py create           phonebook
+    python phonebook.py lookup           name phonebook
+    python phonebook.py add              'full name' 'number' phonebook
+    python phonebook.py change           'full name' 'number' phonebook
+    python phonebook.py remove           'full name' phonebook
+    python phonebook.py reverse-lookup   'full name' phonebook
+"""
 
-def create_phonebook(phonebook_name):
-    """Create a new phonebook.
-    Args:
-        phonebook_name (str): the name of the phonebook to create
-    """
-    pass
+# Store valid commands and the number of arguments they take
+VALID_COMMANDS = {'create': 1,
+                  'lookup': 2,
+                  'add': 3,
+                  'change': 3,
+                  'remove': 2,
+                  'reverse-lookup': 2}
 
+def create(args):
+    """Creates a new phonebook textfile"""
+    phonebook = args[1]
+    # Create empty phonebook
+    with open(phonebook, 'w') as f:
+        return ['Sucessfully created %s.\n' % phonebook]
 
-def add_entry(name, number, phonebook_name):
-    """Add a new name and number to the given phonebook.
-    Args:
-        name (str): name of the entry to add
-        number (str): phone number of the entry to add
-        phonebook_name (str): name of the phonebook
-    """
-    pass
+def lookup(args):
+    """Returns entries in the phonebook containing the given name"""
+    name = args[1]
+    phonebook = args[2]
+    try:
+        with open(phonebook) as f:
+            return [line for line in f if line.index(name) >= 0]
+    except IOError:
+        return ['Error: no such phonebook.']
+    except ValueError:
+        return ['Error: %s not found.' % name]
 
+def add(args):
+    """Adds an entry to the phonebook"""
+    name = args[1]
+    number = args[2]
+    phonebook = args[3]
+    with open(phonebook, 'a') as f:
+        f.write('%s %s\n' % (name, number))
+        return ["Successfully added %s." % name]
 
-def update_entry(name, new_number, phonebook_name):
-    """Update an entry with the given name in the given phonebook
-    with the new phone bumber.
-    Args:
-        name (str): name of the entry to update
-        new_number (str): new phone number of the entry
-        phonebook_name (str): name of the phonebook
-    """
-    pass
+def change_or_remove(args):
+    """Changes or removes an entry in the phonebook"""
+    command = args[0].lower()
+    name = args[1]
+    phonebook = args[-1]
+    try:
+        os.rename(phonebook, 'temp.pb')
+        with open('temp.pb') as f_in, open(phonebook, 'w') as f_out:
+            for line in f_in:
+                if line.index(name) >= 0:
+                    # Write information to file if changing
+                    if command == 'change':
+                        number = args[2]
+                        f_out.write(('%s %s\n' % (name, number)))
+                    # Ignore if removing
+                else:
+                    f_out.write(line)
+        os.remove('temp.pb')
+        if command == 'change':
+            return ['Successfully changed %s.\n' % name]
+        else:
+            return ['Successfully removed %s.\n' % name]
+    except OSError:
+        return ['Error: no such phonebook.']
+    except ValueError:
+        return ['Error: %s not found.' % name]
 
+def validate_args(args):
+    """Validates the number of arguments"""
+    command = args[0]
+    args_length = len(args) - 1
+    return VALID_COMMANDS[command] == args_length
 
-def remove_entry(name, phonebook_name):
-    """Remove an entry with the given name in the given phonebook.
-    Args:
-        name (str): name of the entry to delete
-        phonebook_name (str): name of the phonebook
-    """
-    pass
+def validate_command(command):
+    """Validates the command name"""
+    return command in list(VALID_COMMANDS.keys())
 
-
-def lookup_name(name, phonebook_name):
-    """Look up an entry by name in the given phonebook.
-    Args:
-        name (str): name to look up
-        phonebook_name (str): name of the phonebook
-    """
-    pass
-
-
-def lookup_number(number, phonebook_name):
-    """Look up an entry by number in the given phonebook.
-    Args:
-        number (str): phone number to look up
-        phonebook_name (str): name of the phonebook
-    """
-    pass
-
+def main(args=sys.argv[1:]):
+    # Command line processing
+    if len(args) == 0:
+        print(USAGE)
+        return 1
+    # Command validation
+    command = args[0].lower()
+    valid = validate_command(command)
+    if not valid:
+        print(('Error: %s is not a valid command.' % command))
+        return 1
+    # Argument validation
+    valid = validate_args(args)
+    if not valid:
+        print(('Error: expected %d argument(s) for the %s command'
+               % (VALID_COMMANDS[command], command)))
+        return 1
+    commands = {
+        'create': create,
+        'lookup': lookup,
+        'add': add,
+        'change': change_or_remove,
+        'remove': change_or_remove,
+        'reverse-lookup': lookup
+    }
+    # Call appropriate function and print results
+    for line in commands[command](args):
+        print(line, end=' ')
+    return 0
 
 if __name__ == '__main__':
-    args = sys.argv[:]
-    script = args.pop(0)    # name of script is first arg
-    if not args:
-        print("Use phonebook.py --help, if you're not sure how to use this program.")
-        quit()
-    command = args.pop(0)   # the next arg will be the main command
-
-    if command == 'create':
-        if len(args) != 1:
-            print("Phonebook name required")
-            quit()
-        phonebook_name = args.pop(0)
-        create_phonebook(phonebook_name)
-
-    elif command == 'add':
-        if len(args) != 3:
-            print("Name, number, and phonebook name required")
-            quit()
-        name, number, phonebook_name = args
-        add_entry(name, number, phonebook_name)
-
-    elif command == 'update':
-        if len(args) != 3:
-            print("Name, number, and phonebook name required")
-            quit()
-        name, new_number, phonebook_name = args
-        update_entry(name, new_number, phonebook_name)
-
-    elif command == 'remove':
-        if len(args) != 2:
-            print("Name and phonebook name required")
-            quit()
-        name, phonebook_name = args
-        remove_entry(name, phonebook_name)
-
-    elif command == 'lookup':
-        if len(args) != 2:
-            print("Name and phonebook name required")
-            quit()
-        name, phonebook_name = args
-        lookup_name(name, phonebook_name)
-
-    elif command == 'reverse-lookup':
-        if len(args) != 2:
-            print("Number and phonebook name required")
-            quit()
-        phonebook_name, phonebook_name = args
-        lookup_number(number, phonebook_name)
-
-    elif command == '--help':
-        print('''
-
-create_phonebook(phonebook_name):
-    Create a new phonebook.
-        Arguments:
-        phonebook_name (str): the name of the phonebook to create
-
-
-add_entry(name, number, phonebook_name):
-    Add a new name and number to the given phonebook.
-        Arguments:
-        name (str): name of the entry to add
-        number (str): phone number of the entry to add
-        phonebook_name (str): name of the phonebook
-
-
-update_entry(name, new_number, phonebook_name):
-    Update an entry with the given name in the given phonebook
-        with the new phone bumber.
-        Arguments:
-            name (str): name of the entry to update
-        new_number (str): new phone number of the entry
-        phonebook_name (str): name of the phonebook
-
-
-
-remove_entry(name, phonebook_name):
-    Remove an entry with the given name in the given phonebook.
-        Arguments:
-            name (str): name of the entry to delete
-        phonebook_name (str): name of the phonebook
-
-
-
-lookup_name(name, phonebook_name):
-    Look up an entry by name in the given phonebook.
-        Arguments:
-        name (str): name to look up
-        phonebook_name (str): name of the phonebook
-
-
-
-lookup_number(number, phonebook_name):
-    Look up an entry by number in the given phonebook.
-        Arguments:
-        number (str): phone number to look up
-        phonebook_name (str): name of the phonebook
-        ''')
-
-    else:
-        print("Invalid command")
+    status = main()
+    sys.exit(status)
